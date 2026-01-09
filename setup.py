f50804a5005a21dbda483130cf099191544a07e0
crosschainer <eodkwokm@gmail.com>
2026-01-09 10:36:41 +0100

    Fix macOS universal build flags

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import platform
 import sys
 
 from setuptools import Extension, find_packages, setup
@@ -8,15 +7,10 @@ from setuptools import Extension, find_packages, setup
 
 def _ext_modules() -> list[Extension]:
     extra_args: list[str] = []
-    machine = platform.machine().lower()
     if sys.platform == "win32":
         extra_args.append("/O2")
     else:
         extra_args.extend(["-O3", "-funroll-loops"])
-        if "arm" in machine or "aarch64" in machine:
-            extra_args.append("-march=armv8-a+crypto")
-        if "x86" in machine or "amd64" in machine or "i386" in machine:
-            extra_args.append("-msha")
     return [
         Extension(
             "baseline_miner._sha256d",
